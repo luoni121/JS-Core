ad0aee1b846e63c364bd5c4ef3c92c67ec008970
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-10-07 23:03:27 +0300

    Update Objects and Json.js

--- Exercise 6 - Objects and Json/Objects and Json.js
+++ Exercise 6 - Objects and Json/Objects and Json.js
@@ -1,1 +1,51 @@
+function p01HeroicInventory(input) {
+    let obj = input.map(e => {
+        let tokens = e.split(' / ');
+
+        let itemsArr = [];
+        if (tokens.length > 2) {
+            itemsArr = tokens.pop().split(', ');
+        }
+
+        return {
+            name: tokens.shift(),
+            level: Number(tokens.shift()),
+            items: itemsArr
+        }
+    });
+    console.log(JSON.stringify(obj));
+}
+// p01HeroicInventory(['Isacc / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antara']);
+
+/*******************************************************************
+***************************/
+
+function p02JSONtable(input) {
+    let html = '<table>\n';
+
+    for(let row of input){
+        let item = JSON.parse(row)
+        html += '  <tr>\n'
+        html += '     <td>' + item.name + '</td>\n'
+        html += '     <td>' + item.position + '</td>\n'
+        html += '     <td>' + item.salary + '</td>\n'
+        html += '  <tr>\n'
+    }
+    html += '</table>'
+    
+    console.log(html);
+}
+// p02JSONtable(['{"name":"Pesho","position":"Promenliva","salary":100000}',
+//     '{"name":"Teo","position":"Lecturer","salary":1000}',
+//     '{"name":"Georgi","position":"Lecturer","salary":1000}']);
+
+/*******************************************************************
+***************************/
+
+
+
+
+
 
