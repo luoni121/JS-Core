545c32d610f5ed3a5b68d204ace5fee5092bbd33
luoni121 <36385236+luoni121@users.noreply.github.com>
2018-10-07 23:52:38 +0300

    Update Objects and Json.js

--- Exercise 6 - Objects and Json/Objects and Json.js
+++ Exercise 6 - Objects and Json/Objects and Json.js
@@ -44,6 +44,44 @@ function p02JSONtable(input) {
 /*******************************************************************
 ***************************/
 
+function p03CappyJuice(input) {
+    let juices = input.map(e => {
+        let temp = e.split(' => ');
+
+        return {
+            juice: temp[0],
+            quantity: Number(temp[1])
+        }
+    });
+
+    let map = new Map();
+    juices.forEach(item => {
+        if (map.has(item.juice) == false) {
+            map.set(item.juice, 0);
+        }
+        let totalQuantity = map.get(item.juice) + item.quantity;
+
+        if (map.get(item.juice) < 1000) {
+            map.delete(item.juice);
+            map.set(item.juice, 0);
+        }
+        map.set(item.juice, totalQuantity);
+    });
+
+    for (var [key, value] of map.entries()) {
+        let bottles = Math.floor(value / 1000);
+        console.log(key + ' => ' + bottles);
+    }
+}
+// p03CappyJuice(['Kiwi => 234',
+//     'Pear => 2345',
+//     'Watermelon => 3456',
+//     'Kiwi => 4567',
+//     'Pear => 5678',
+//     'Watermelon => 6789']);
+
+/*******************************************************************
+***************************/
 
 
 
